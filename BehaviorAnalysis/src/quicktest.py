ac3b1fd176387b314222814d3ded6f144c3d4955
vasko <sp0x@xakep.ru>
2017-08-14 12:54:06 +0300

    Aggregated user features

--- BehaviorAnalysis/src/quicktest.py
+++ BehaviorAnalysis/src/quicktest.py
@@ -25,9 +25,15 @@ weeksAvailable = collection.find({
 weeksAvailable.sort()
 
 #our test week
+weekLimit = 4
+weeksAvailable = weeksAvailable[ (-1) * weekLimit:] #last n weeks
 lastWeek = weeksAvailable.pop()
 targetData = []
 inputData = []
+limit = 2 * 100 * 1000
+userlimit = 15 * 1000
+
+
 for index, week in enumerate(weeksAvailable):
     if index== (len(weeksAvailable) -1 ):
         next_week = lastWeek
@@ -35,48 +41,57 @@ for index, week in enumerate(weeksAvailable):
         next_week = weeksAvailable[index+1]
     print "Preparing week: " + str(week) + "    " + str(index+1) + "/" + str(len(weeksAvailable))
     next_week_purchases = dict()
-    weekData = collection.find({
-        "UserId" : appId,
-        "TypeId" : userTypeId,
-        "Document.g_timestamp" : {'$gte': week, '$lt': next_week}
-    }, { 
-        "Document.uuid" : 1,
-        "Document.noticed_date" : 1,
-        "Document.is_paying" : 1,
-        "Document.visits_on_weekends" : 1,
-        "Document.p_online_weekend" : 1,
-        "Document.days_visited_ebag" : 1,
-        "Document.time_spent_ebag" : 1,
-        "Document.time_spent_on_mobile_sites" : 1,
-        "Document.mobile_visits" : 1,
-        "Document.visited_ebag" : 1,
-        "Document.p_buy_age_group" : 1,
-        "Document.p_buy_gender_group" : 1,
-        "Document.p_visit_ebag_age" : 1,
-        "Document.p_visit_ebag_gender" : 1,
-        "Document.p_to_go_online" : 1,
-        "Document.avg_time_spent_on_high_pageranksites" : 1,
-        "Document.highranking_page_0" : 1,
-        "Document.highranking_page_1" : 1,
-        "Document.highranking_page_2" : 1,
-        "Document.highranking_page_3" : 1,
-        "Document.highranking_page_4" : 1,
-        "Document.time_spent_online" : 1,
-        "path_similarity_score" : 1,
-        "path_similarity_score_time_spent" : 1
-    }).batch_size(10000)
+
+    pipeline = [
+        {"$match" : { 
+            "TypeId" : userTypeId, 
+            "UserId" : appId,
+            "Document.g_timestamp" : {'$gte': week, '$lt': next_week},
+         }
+        },
+        {"$group": {
+            "_id": {
+                "uuid" : "$Document.uuid",
+                "week_start" : "$Document.g_timestamp"
+            }, 
+            "visits_count": {"$sum": 1}, 
+                "visits_on_weekends" : { "$avg" : "$Document.visits_on_weekends"},
+                "p_online_weekend" : { "$avg" : "$Document.p_online_weekend"},
+                "days_visited_ebag" : { "$avg" : "$Document.days_visited_ebag"},
+                "time_spent_ebag" : { "$avg" : "$Document.time_spent_ebag"},
+                "time_spent_on_mobile_sites" : { "$avg" : "$Document.time_spent_on_mobile_sites"},
+                "mobile_visits" : { "$avg" : "$Document.mobile_visits"},
+                "visited_ebag" : { "$avg" : "$Document.visited_ebag"},
+                "p_buy_age_group" : { "$avg" : "$Document.p_buy_age_group"},
+                "p_buy_gender_group" : { "$avg" : "$Document.p_buy_gender_group"},
+                "p_visit_ebag_age" : { "$avg" : "$Document.p_visit_ebag_age"},
+                "p_visit_ebag_gender" : { "$avg" : "$Document.p_visit_ebag_gender"},
+                "p_to_go_online" : { "$avg" : "$Document.p_to_go_online"},
+                "avg_time_spent_on_high_pageranksites" : { "$avg" : "$Document.avg_time_spent_on_high_pageranksites"},
+                "highranking_page_0" : { "$avg" : "$Document.highranking_page_0"},
+                "highranking_page_1" : { "$avg" : "$Document.highranking_page_1"},
+                "highranking_page_2" : { "$avg" : "$Document.highranking_page_2"},
+                "highranking_page_3" : { "$avg" : "$Document.highranking_page_3"},
+                "highranking_page_4" : { "$avg" : "$Document.highranking_page_4"},
+                "time_spent_online" : { "$avg" : "$Document.time_spent_online"},
+                "path_similarity_score" : { "$avg" : "$path_similarity_score"},
+                "path_similarity_score_time_spent" : { "$avg" : "$path_similarity_score_time_spent"}
+            
+        }}]
+    weekData = collection.aggregate(pipeline)     
     next_week_end = next_week + timedelta(days=7)
     next_week_purchases = collection.find({        
         "TypeId" :  userTypeId,
         "UserId" : appId,
         "Document.noticed_date" : { "$gte" : next_week, "$lt" : next_week_end  }
     }).distinct("Document.uuid")
-    print "CrWeek Visits: {0} NxWeek Purchased users: {1}".format(weekData.count(), len(next_week_purchases))
-    for week_session in weekData: 
-        tmpDoc = week_session["Document"]
-        uuid = tmpDoc["uuid"]
-        simscore = 0 if not "path_similarity_score" in week_session else  week_session["path_similarity_score"] 
-        simtime = 0 if not "path_similarity_score_time_spent" in week_session else  week_session["path_similarity_score_time_spent"]
+    weekData = list(weekData)
+    print "CrWeek Visits: {0} NxWeek Purchased users: {1}".format(len(weekData), len(next_week_purchases))
+    for week_session in weekData:
+        tmpDoc = week_session #["Document"] 
+        uuid = tmpDoc["_id"]["uuid"]
+        simscore = 0 if not "path_similarity_score" in tmpDoc else  tmpDoc["path_similarity_score"] 
+        simtime = 0 if not "path_similarity_score_time_spent" in tmpDoc else  tmpDoc["path_similarity_score_time_spent"]
         inputData.append([
             tmpDoc["visits_on_weekends"],
             tmpDoc["p_online_weekend"],
