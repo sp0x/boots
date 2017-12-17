e50afcdfcb44edec99604eb8badd5dd8e66a5c55
vasko <sp0x@xakep.ru>
2017-12-17 17:07:57 +0200

    Added management controllers.

---
+++ Netlyt.Web/Models/ManageViewModels/RemoveLoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace Netlyt.Web.Models.ManageViewModels
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
---
+++ Netlyt.Web/Models/ManageViewModels/TwoFactorAuthenticationViewModel.cs
@@ -0,0 +1,11 @@
+namespace Netlyt.Web.Models.ManageViewModels
+{
+    public class TwoFactorAuthenticationViewModel
+    {
+        public bool HasAuthenticator { get; set; }
+
+        public int RecoveryCodesLeft { get; set; }
+
+        public bool Is2faEnabled { get; set; }
+    }
+}
