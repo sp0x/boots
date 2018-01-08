35fa04c5d2a9b8e3db6cf61e56557c290b1a0dec
vasko <sp0x@xakep.ru>
2018-01-08 20:22:45 +0200

    Removed local orion files.

--- Orion/src/envir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export mongo_pass="Y8Iwb6lI4gRdA+tbsaBtVj0sIRVuUedCOJfNyD4hymuRqG4WVNlY9BfQzZixm763"
-export mongo_host="mongo.netlyt.com"
-export mongo_user="vasko"
-export mongo_port="27017"
-export user_type="59cbc103003e730508e87c2c"
-export app_id="123123123"
-export mongo_db="netvoid"
